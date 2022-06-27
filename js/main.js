85e94e2939be1597e9461bb17032e46c986a99b3
Nataliia-Zemlyak <znatashaa13@gmail.com>
2022-06-27 16:00:40 +0300

    burger

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let hamburger = document.querySelector(".hamburger")
+let menu = document.querySelector(".menu")
+
+hamburger.onclick = function () {
+    menu.classList.toggle ("active-burger")
+}
+
+
+
+
+
+
